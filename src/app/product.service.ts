bd977f2f9de5cd330c7e6cae536c8d069214f18b
Mayur Meshram <mmeshram43@gmail.com>
2021-12-20 13:23:11 +0530

    Added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http,Response  } from "@angular/http";
 
 @Injectable()
 export class ProductService {
 
-  constructor() { }
+  constructor( private _http : Http ) { }
+  _albumUrl = '../assets/album.json';
+
+  getAlbum(id : number){
+    return this._http.get(this._albumUrl);
+
+  }
 
 }
